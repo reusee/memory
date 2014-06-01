5fd4569abcb0015f3313dccf19a3030d7605e96e
reus <reusee@gmail.com>
2014-06-01 22:58:01 +0800

    updates

--- main.go
+++ main.go
@@ -21,8 +21,6 @@ import (
 	"strings"
 	"time"
 	"unsafe"
-
-	termbox "github.com/nsf/termbox-go"
 )
 
 var rootPath string
@@ -352,30 +350,6 @@ func main() {
 		fmt.Printf("unknown command\n")
 		os.Exit(0)
 	}
-}
-
-func p(x, y int, text string) {
-	for _, r := range text {
-		termbox.SetCell(x, y, r, termbox.ColorDefault, termbox.ColorDefault)
-		x += wcwidth(r)
-	}
-	termbox.Flush()
-}
-
-func wcwidth(r rune) int {
-	if r >= 0x1100 &&
-		(r <= 0x115f || r == 0x2329 || r == 0x232a ||
-			(r >= 0x2e80 && r <= 0xa4cf && r != 0x303f) ||
-			(r >= 0xac00 && r <= 0xd7a3) ||
-			(r >= 0xf900 && r <= 0xfaff) ||
-			(r >= 0xfe30 && r <= 0xfe6f) ||
-			(r >= 0xff00 && r <= 0xff60) ||
-			(r >= 0xffe0 && r <= 0xffe6) ||
-			(r >= 0x20000 && r <= 0x2fffd) ||
-			(r >= 0x30000 && r <= 0x3fffd)) {
-		return 2
-	}
-	return 1
 }
 
 type ConnectSorter struct {
